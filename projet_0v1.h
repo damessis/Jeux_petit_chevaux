b96d2cbb77a50cc5a8bdc0a6a10919c38133a263
damessis <115561415+damessis@users.noreply.github.com>
2022-10-13 22:20:45 +0200

    Update projet_0v1.h

--- projet_0v1.h
+++ projet_0v1.h
@@ -1,8 +1,10 @@
+//la structure position permet de stocker la position d'un joeur sur le plateau
+//un plateau est divisé en quatre zone et chaque zone a un certains nombre de cellule
 typedef struct
 { /* data */
     int zone;
-    int case;
+    int cellule;
 } position;
 
-void creerfils();
-void initPlateau(int n);
+void creerfils();//fonction qui permet de creer un joueur
+void initPlateau(int n);//fonction qui cree "n" joueur et initialise un plateau avec eux
