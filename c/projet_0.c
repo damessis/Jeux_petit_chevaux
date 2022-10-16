463d78db6e3fdab4e8019a8329b099d5f853014a
damessis <115561415+damessis@users.noreply.github.com>
2022-10-16 04:12:19 +0200

    Update projet_0.c

--- c/projet_0.c
+++ c/projet_0.c
@@ -13,71 +13,114 @@ struct PositionPion {
 
 struct Joueur {
     int pid;
-    PositionPion pion1;
-    PositionPion pion2;
+    struct PositionPion *pion1;
+    struct PositionPion *pion2;
 }
 
 Joueur* tabJoueur;
 int* tubes;
+/*---------------------------------------------------------------------------------------------------------*/
+
+struct Joueur **createTableDeJoueur(int nbJoueur){
+  struct Joueur **tableauDeJoueur =(struct Joueur **)malloc(nbJoueur*sizeof(struct Joueur *));
+  struct Joueur *joueur=(struct Joueur *)malloc(nbJoueur*sizeof(struct Joueur));
+  struct Position *position1=(struct Position *)malloc(nbJoueur*sizeof(struct Position));
+ struct Position *position2=(struct Position *)malloc(nbJoueur*sizeof(struct Position));
+  for(int i = 0 ; i < nbJoueur ; i++){
+		tableauDeJoueur[i] = &joueur[i];
+	}
+  for(int i = 0 ; i < nbJoueur ; i++){
+    tableauDeJoueur[i][0].pion1 = &position1[i];
+    tableauDeJoueur[i][0].pion2 = &position2[i];
+  }
+  return tableauDeJoueur;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
 
 
-void creerfils(struct Joueur *jx, int* tube,int numerofils){
-	int *pinit=tube;
-	int i=0;
-	int k=0;
-  	switch (fork())
-    	{
+void detruireLesJoueur( struct Joueur **tableauDeJoueur){
+	free(tableauDeJoueur[0]);
+	free(tableauDeJoueur);
+}
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+
+int **createTableDeTubes(int nbJoueur){
+  int **tableauDeTubes=(int **)malloc(sizeof(int*)*nbJoueur);
+  int *tubes = (int *)malloc(sizeof(int)*nbJoueur*2);
+  for(int i = 0 ; i < nbJoueur ; i++){
+		tableauDeTubes[i] = &tubes[i*2];
+	}
+	return tableauDeTubes;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+
+void detruireLesTubes(int **tableauDeTubes){
+	free(tableauDeTubes[0]);
+	free(tableauDeTubes);
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+
+void creerfils(struct Joueur **tableauDeJoueur, int **tableauDeTubes,int n,int nbJoueur){
+    	switch (fork()){
     		case -1:
         		fprintf(stderr, "Erreur du premier fork\n");
         		exit(-1);
     		case 0:
-        		printf("pid: %d   ppid: %d\n", getpid(), getppid() );
-        		close(1);
-        		dup(tube[numerofils][1]);
-			close(0);
-			if(numerofils==0){
-				while(tube+1!=null){
-					tube++;
-				}
-				dup(*tube[0]);
-			tube=pinit+1;
-			}else if(numerofils==3/*nbJoueur*//*longeur du tableau -1 (arguement dans le main*/){
-				close(1);
-                    		dup(tube[numerofils][1]);
-				close(0);
-                    		dup(tube[0][0]);	
-			}else{
-			      dup(tube[numerofils-1][0]);
-			}
-			
-			while(i<4){
-				while(k<2){
-					close(tube[i][j]);
-				}
-			}
-  			exit(0);
-  		default:
-      			wait(NULL);
+          		printf("n %d\n",i);
+          		(tableauDeJoueur[i][0]).pid=getpid();
+          		(tableauDeJoueur[i][0]).pion1->zone=i;
+          		(tableauDeJoueur[i][0]).pion1->cellule=0;
+			(tableauDeJoueur[i][0]).pion2->zone=i;
+          		(tableauDeJoueur[i][0]).pion2->cellule=0;
+          		printf("pid du joueur courant: %d\n", tableauDeJoueur[i]->pid);
+          		printf("position initiale premier pion du joueur courant zone: %d\n", tableauDeJoueur[i][0].pion1->zone);
+          		printf("position initiale premier pion du joueur courant cellule: %d\n", tableauDeJoueur[i][0].pion1->cellule);
+			printf("position initiale deuxieme pion du joueur courant zone: %d\n", tableauDeJoueur[i][0].pion1->zone);
+          		printf("position initiale deuxieme pion du joueur courant cellule: %d\n", tableauDeJoueur[i][0].pion1->cellule);
+          		printf("pid: %d   ppid: %d\n", getpid(), getppid() );
+          		if (pipe(tableauDeTubes[i]) != 0) {/* pipe */
+              			fprintf(stderr, "Erreur dans pipe \n"); 
+              			exit(-1);
+          		}
+            		write(tableauDeTubes[i][1], &ecr, sizeof(int));
+            		close(tableauDeTubes[i][1]);
+            		printf("j'envoie :%d au joueur suivant\n", ecr);
+          		if(i!=0){
+              			if(read(tableauDeTubes[i-1][0], &lu, sizeof(int))>0){
+                			close(tableauDeTubes[i-1][0]);
+                 			printf("je recois :%d du joueur precedent\n", lu);
+              			}else{
+                			fprintf(stderr, "Erreur read \n");
+              			}
+          		}
+          		if(i==nbJoueur-1){
+            			if(read(tableauDeTubes[nbJoueur-1][0], &lu, sizeof(int))>0){
+                			close(tableauDeTubes[nbJoueur-1][0]);
+                 			printf("je recois :%d du joueur precedent\n", lu);
+              			}else{
+                			fprintf(stderr, "Erreur read \n");
+              			}
+          		}  
+  			default:
+            			printf("\n\n");
+      				while (wait(NULL)!=-1);
+ 
   	}
+  			
 }
 
-
-
-void initPlateau(int nbreJoueur){
-
-  /* Allocation de la mémoire pour le tableau de Joueur et de tubes */
-  tabJoueur = (Joueur *)malloc(nbreJoueur * sizeof(Joueur));
-  tubes = (int[2])malloc(nbreJoueur*sizeof(int[2]));
-
-  for (int i = 0; i < nbreJoueur; i++) {
-    tabJoueur[i][2].zone = i; // pion1 dans la zone du joueur
-    tabJoueur[i][2].cellule = -1; // -1 est le cas où le pion1 est encore dans l'écurie
-    tabJoueur[i][3].zone = i; // pion2 dans la zone du joueur
-    tabJoueur[i][3].cellule = -1; // -1 est le cas où le pion2 est encore dans l'écurie
-  }
-
+void initPlateau(int nbJoueur,int **tableauDeTubes,struct Joueur **tableauDeJoueur ){
   /* Boucle for qui crée tous les fils */
   for(int i = 0; i < n; i++){
-    creerfils(tabJoueur, tubes, i);
+    creerfils(tableauDeJoueur, tableauDeTubes,i,nbJoueur);
   }
 }
+
