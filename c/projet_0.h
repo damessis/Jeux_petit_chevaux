5e63fac08533748fe25130e34df231cd88f402c1
damessis <115561415+damessis@users.noreply.github.com>
2022-10-18 00:04:50 +0200

    Update projet_0.h

--- c/projet_0.h
+++ c/projet_0.h
@@ -1,8 +1,18 @@
 /*un plateau est divisé en quatre zone et chaque zone a un certains nombre de cellule 
 la structure position permet d'identifier de maniere unique chaque case du plateau
 et nous permettra ainsi de reperer l'endroit ou se trouve un joueur;*/
-#ifndef PROJET_0
-#define PROJET_0
+struct Position{ /* data */
+    int zone;
+    int cellule;
+};
+
+/*------------------------------------------------------------------------------------------------------------------*/
+/*la structure joueur permet de stocker les informations sur un joueur a savoir son pid et sa position*/
+struct Joueur{ /* data */
+    int pid;
+    struct Position *pion1;
+    struct Position *pion2;
+};
 
 /*------------------------------------------------------------------------------------------------------------------*/
 void creerfils(struct Joueur **tableauDeJoueur, int **tableauDeTubes, int n, int nbJoueur, int* ecr,int* lu );//fonction qui permet de creer un joueur
@@ -21,4 +31,3 @@ struct Joueur **createTableDeJoueur(int nbJoueur);//cette fonction nous permet d
 
 /*------------------------------------------------------------------------------------------------------------------*/
 void detruireLesJoueur( struct Joueur **tableauDeJoueur);//cette fonction nous permet en fin de parti de nous debarasser des informatiosn sur les joueur en meme temps que nous les tuons;
-#endif
