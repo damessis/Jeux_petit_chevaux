835d914db2d45e9c87595145702db6c30432bf9d
damessis <115561415+damessis@users.noreply.github.com>
2022-10-19 20:30:38 +0200

    Update projet_0.h

--- c/projet_0.h
+++ c/projet_0.h
@@ -1,33 +1,37 @@
-/*un plateau est divisé en quatre zone et chaque zone a un certains nombre de cellule 
-la structure position permet d'identifier de maniere unique chaque case du plateau
-et nous permettra ainsi de reperer l'endroit ou se trouve un joueur;*/
-struct Position{ /* data */
-    int zone;
-    int cellule;
+#ifndef PROJET_0
+#define PROJET_0
+
+/*------------------------------------------------------------------------------------------------------------------*/
+
+
+/*chaque pion est detient certaines informations pour l'identifierde maniere unique*/
+struct Joueur {
+		pid_t pid; // le nom du joueur a qui appartient ce pion en l'occurence dans notre programme les joueur sont des processus fils du programme main 
+    //chaque joueur a 2 pion et chaque pion est identifié par sa zone et sa case 
+    int pion1[2];//a l'indice 0 on stocke la zone du pion et a l'indice 1 on stocke la case
+    int pion2[2];
 };
+ 
+ 
+/*------------------------------------------------------------------------------------------------------------------*/
+
+
+//fonction qui permet de creer un joueur
+void initialiser_joueur(struct Joueur tableauJoueur[], int joueur, pid_t pid);
 
 /*------------------------------------------------------------------------------------------------------------------*/
-/*la structure joueur permet de stocker les informations sur un joueur a savoir son pid et sa position*/
-struct Joueur{ /* data */
-    int pid;
-    struct Position *pion1;
-    struct Position *pion2;
-};
+
+
+int de(int dernier_lance);
+
+
 
 /*------------------------------------------------------------------------------------------------------------------*/
-void creerfils(struct Joueur **tableauDeJoueur, int **tableauDeTubes, int n, int nbJoueur, int* ecr,int* lu );//fonction qui permet de creer un joueur
+
+
+//fonction qui associe les joueur et leur pions(initialisé) a un processus
+void initPlateau(int nbJoueur,struct Joueur tableauDeJoueur[],int tableauDeTubesFils[][],int tableauDeTubesPere[][]);
+
 
 /*------------------------------------------------------------------------------------------------------------------*/
-void initPlateau(int n, int **tableauDeTubes,struct Joueur **tableauDeJoueur );//fonction qui cree "n" joueur et initialise un plateau avec eux
-
-/*------------------------------------------------------------------------------------------------------------------*/
-int **createTableDeTubes(int nbJoueur);//permet de stocker tous les tubes en fonction du nombre de joueur et de pouvoir y acceder par reference
-
-/*------------------------------------------------------------------------------------------------------------------*/
-void detruireLesTubes(int **tableauDeTubes);//cette fonction nous permettra de detruire tous les tubes une fois qu'on en aura plus besoin et ainsi eviter d'occuper inutilement de la memoire
-
-/*------------------------------------------------------------------------------------------------------------------*/
-struct Joueur **createTableDeJoueur(int nbJoueur);//cette fonction nous permet de stocker les informations de tous les joueur;
-
-/*------------------------------------------------------------------------------------------------------------------*/
-void detruireLesJoueur( struct Joueur **tableauDeJoueur);//cette fonction nous permet en fin de parti de nous debarasser des informatiosn sur les joueur en meme temps que nous les tuons;
+#endif
