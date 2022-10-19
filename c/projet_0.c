af4a29e40151fa48a5d4b94d948160031c718138
damessis <115561415+damessis@users.noreply.github.com>
2022-10-19 20:32:24 +0200

    Update projet_0.c

--- c/projet_0.c
+++ c/projet_0.c
@@ -1,68 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/wait.h>
 #include "projet_0v1.h"
+#include <stdbool.h>
 
-struct Joueur **createTableDeJoueur(int nbJoueur){
-  struct Joueur **tableauDeJoueur =(struct Joueur **)malloc(nbJoueur*sizeof(struct Joueur *));
-  struct Joueur *joueur=(struct Joueur *)malloc(nbJoueur*sizeof(struct Joueur));
-  struct Position *position1=(struct Position *)malloc(nbJoueur*sizeof(struct Position));
- struct Position *position2=(struct Position *)malloc(nbJoueur*sizeof(struct Position));
-  for(int i = 0 ; i < nbJoueur ; i++){
-		tableauDeJoueur[i] = &joueur[i];
-	}
-  for(int i = 0 ; i < nbJoueur ; i++){
-    tableauDeJoueur[i][0].pion1 = &position1[i];
-    tableauDeJoueur[i][0].pion2 = &position2[i];
-  }
-  return tableauDeJoueur;
-}
 
 /*---------------------------------------------------------------------------------------------------------*/
 
+bool partieGagne=false;
+pid_t pid;
+pid_t pidJoueurSuivant;
 
-void detruireLesJoueur( struct Joueur **tableauDeJoueur){
-	free(tableauDeJoueur[0]);
-	free(tableauDeJoueur);
+int de(int dernier_lance){
+	int a;
+	srand(time(NULL));
+	a=(rand()+dernier_lance)%6;
+return a+1;// oui on a rarement vu des des faire 0. donc +1
 }
 
 
 /*---------------------------------------------------------------------------------------------------------*/
 
 
-int **createTableDeTubes(int nbJoueur){
-  int **tableauDeTubes=(int **)malloc(sizeof(int*)*nbJoueur);
-  int *tubes = (int *)malloc(sizeof(int)*nbJoueur*2);
-  for(int i = 0 ; i < nbJoueur ; i++){
-		tableauDeTubes[i] = &tubes[i*2];
-	}
-	return tableauDeTubes;
+
+void initialiser_joueur(struct Joueur tableauJoueur[],int joueur,pid_t pid){
+  tableauJoueur[joueur].pid=pid;
+  tableauJoueur[joueur].pion1[0]=joueur;
+  tableauJoueur[joueur].pion1[1]=-1;
+  tableauJoueur[joueur].pion2[0]=joueur;
+  tableauJoueur[joueur].pion2[1]=-1;
 }
 
-/*---------------------------------------------------------------------------------------------------------*/
 
-
-void detruireLesTubes(int **tableauDeTubes){
-	free(tableauDeTubes[0]);
-	free(tableauDeTubes);
-}
 
 /*---------------------------------------------------------------------------------------------------------*/
 		
 
 
-void initPlateau(int nbJoueur,int **tableauDeTubes,struct Joueur **tableauDeJoueur ){
-  int ecr=1;
-  int lu=100;
-  pid_t pid;
-  
-  for(int i=0;i<nbJoueur;i++){
-    if (pipe(tableauDeTubes[i]) != 0) {/* pipe */
- 			 fprintf(stderr, "Erreur dans pipe \n"); 
- 			 exit(-1);
-   	}
-  }
+void initPlateau(int nbJoueur, struct Joueur tableauDeJoueur[], int tableauDeTubesFils[][],int tableauDeTubesPere[][]){
 
   for(int i=0;i<nbJoueur;i++){
     pid = fork();
@@ -70,40 +47,89 @@ void initPlateau(int nbJoueur,int **tableauDeTubes,struct Joueur **tableauDeJoue
                 printf ("PID du PERE %d\n",getpid());
                 printf ("Création JOUEUR #%d : PID %d\n",i,pid);
                 wait(NULL);
-    }
-    else if (!pid){
-              
-              if(i!=0){
-              			if(read(tableauDeTubes[i-1][0], &lu, sizeof(int))>=0){
-                			//close(tableauDeTubes[i-1][0]);
-                 			printf("je recois :%d du joueur precedent\n", lu);
-              			}else{
-                			fprintf(stderr, "Erreur read \n");
-              			}
-          		}
-      	      (tableauDeJoueur[i][0]).pid=getpid();
-          		(tableauDeJoueur[i][0]).pion1->zone=i;
-          		(tableauDeJoueur[i][0]).pion1->cellule=0;
-			        (tableauDeJoueur[i][0]).pion2->zone=i;
-          		(tableauDeJoueur[i][0]).pion2->cellule=0;
-		          printf("pid du joueur courant: %d\n", tableauDeJoueur[i]->pid);
-          		printf("position initiale premier pion du joueur courant zone: %d\n", tableauDeJoueur[i][0].pion1->zone);
-          		printf("position initiale premier pion du joueur courant cellule: %d\n", tableauDeJoueur[i][0].pion1->cellule);
-			        printf("position initiale deuxieme pion du joueur courant zone: %d\n", tableauDeJoueur[i][0].pion1->zone);
-          		printf("position initiale deuxieme pion du joueur courant cellule: %d\n", tableauDeJoueur[i][0].pion1->cellule);
-          		printf("pid: %d   ppid: %d\n", getpid(), getppid() );
-          		write(tableauDeTubes[i][1], &ecr, sizeof(int));
-          		//close(tableauDeTubes[i][1]);
-          		printf("j'envoie :%d au joueur suivant\n", ecr);
-             printf("\n\n");
-             
-          		
+    }else if (!pid){
+              initialiser_joueur(tableauDeJoueur,i,getpid());
+              printf ("Je suis le joueur %d et mon pid est: %d\n",i,tableauDeJoueur[i].pid);
        break;
     }else{
     perror ("Erreur à l'appel de fork()");
     }
-    ecr++;
   }
+  while(!partieGagne){
+     if(getpid()==tableauDeJoueur[0].pid){
+        if(getpid()==pidJoueurSuivant){
+        
+        }else{
+        
+        }
+     }else if(getpid()==tableauDeJoueur[1].pid){
+       if(getpid()==pidJoueurSuivant){
+        
+       }else{
+        
+       }
+     }else if(getpid()==tableauDeJoueur[2].pid){
+       if(getpid()==pidJoueurSuivant){
+        
+       }else{
+        
+       }
+     }else if(getpid()==tableauDeJoueur[3].pid){
+       if(getpid()==pidJoueurSuivant){
+        
+       }else{
+        
+       }
+     }else{
+     
+     }
+     
+     
+      
+      //fait write dans tube 0
+     }
+     }
+    }
+    partieGagne=true;
+  }
+  
 }
 
+
+/*---------------------------------------------------------------------------------------------------------*/
+
+
+
+int main(int argc, char *argv[]){
+   int nbJoueur;
+   if(argc!=2){
+     fprintf(stderr,"Erreur nombre d'argument incorrect Format correct <nomprogramme> <nombre de joueurs>\n");
+     exit(-1);
+   }else{
+     nbJoueur=atoi(argv[1]);
+     if((nbJoueur!=2) && (nbJoueur!=4)){
+        fprintf(stderr,"Nombre de joueur choisi incorrect Format correct.Pour une partie il faut soit '2 joueurs' soit '4 joueurs'\n");
+        exit(-1); 
+     } 
+   }
+   int tableauDeTubesFils[nbJoueur][2];// on stocke ici les tubes qui permette aux fils de communiquer entre eux*
+   struct Joueur tableauDeJoueur[nbJoueur];
+   int tableauDeTubesPere[nbJoueur][2];// on stocke ici les tubes qui permette au pere d'envoyer des informations aux fils et d'en recevoir
+   for(int i=0;i<nbJoueur;i++){
+    if (pipe(tableauDeTubesFils[i]) != 0) {/* pipe */
+ 			 fprintf(stderr, "Erreur dans pipe \n"); 
+ 			 exit(-1);
+   	}
+   }
+   for(int i=0;i<nbJoueur;i++){
+    if (pipe(tableauDeTubesPere[i]) != 0) {/* pipe */
+ 			 fprintf(stderr, "Erreur dans pipe \n"); 
+ 			 exit(-1);
+   	}
+   }
+   initPlateau(nbJoueur,tableauDeJoueur,tableauDeTubesFils,tableauDeTubesPere);
+   
+   
+     
+}
 
